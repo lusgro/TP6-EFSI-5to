dc560be4322e9a211fda7ac0bc7d9a3f1261374b
lusgro <lucasurielrosenbaum@gmail.com>
2024-07-03 10:27:26 -0300

    TP7

--- src/CitaContext.js
+++ src/CitaContext.js
@@ -1,20 +1,25 @@
 "use client"
 
-import { createContext, useState } from 'react';
+import { createContext, useState, useEffect, useContext } from 'react';
 import Cita from './models/Cita';
 
 export const CitaContext = createContext();
 
 export function CitaProvider({ children }) {
-    const [citas, setCitas] = useState([
-        new Cita("Nina", "Martin", "2021-08-05", "08:20", "Le duele la pierna"),
-        new Cita("Sifon", "Flecha", "2023-08-05", "09:24", "Duerme mucho"),
-        new Cita("Floki", "Ari", "2023-08-05", "16:15", "No está comiendo")
-    ]);
+    const [citas, setCitas] = useState([]);
+
+    useEffect(()=> {
+        const citasGuardadas = localStorage.getItem('citas');
+        setCitas(citasGuardadas ? JSON.parse(citasGuardadas) : []);
+    }, [])
+
+    useEffect(() => {
+        localStorage.setItem('citas', JSON.stringify(citas));
+    }, [citas]);
 
     return (
         <CitaContext.Provider value={{ citas, setCitas }}>
             {children}
         </CitaContext.Provider>
-    )
+    );
 }
